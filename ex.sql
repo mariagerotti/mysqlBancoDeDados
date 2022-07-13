5e4ec83ece6f3815049df166d00e8032b6aa4cbc
mariagerotti <99097680+mariagerotti@users.noreply.github.com>
2022-07-13 15:11:15 -0300

    Add files via upload

---
+++ ex.sql
@@ -0,0 +1,35 @@
+
+/*insert into produtos (desc_produto, preco_unitario, preco_embalagem, qtd_embalagem)
+values
+("Notebook Sansung", 7865.35, 7865.35,1),
+("Macbook", 17900.25, 17900.25,1),
+("Televisão LG", 3000.00, 3000.00,1),
+("Celular Sansung j5", 700.00, 700.00,1),
+("Mouse DELL", 300.99, 300.99,1),
+("Cadeira Escritório", 1000.25, 1000.25,1);
+
+
+insert into comentario (cod_cliente, cod_produto, texto, titulo)
+values
+(1,4, "Demais!!!", "Comentário"),
+(8,2, "Horrendo!MUITO RUIM", "Comentário"),
+(9,5, "Gastei meu dinheiro atoa, af:/", "Comentário"),
+(10,1,"Otimo, compraria de novo!", "Comentário"),
+(11,3, "Não comprem essa bomba plmdds!!!!!!", "Comentário"),
+(12,6, "Show.", "Comentário");*/
+
+insert into fisica(cpf, rg, cod_cliente)
+VALUES
+("123.456.789-10", "789.654-96", 1),
+("321.645.987-87", "582.147-98", 8),
+("147.258.369-23", "582.254-36", 9),
+("369.258.147-00", "321.456-08", 10),
+("014.582.693-55", "036.985-71", 11),
+("963.582.471-88", "321.658-97", 12);
+
+INSERT into pedidos()
+insert INTO itens_pedido (cod_produto, cod_pedido, qtd_produto, situação)
+VALUES
+(1,)
+
+
